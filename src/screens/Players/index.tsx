27d9f392b69ee5ed4a9f448f5ae1f357648cbf52
Daniel Fernando <daniel_fmpc@hotmail.com.br>
2025-04-27 18:32:25 -0300

    Adiciona funcionalidade de gerenciamento de jogadores na tela, incluindo seleção de time, exibição da lista de jogadores com opção de remoção e contagem de participantes, utilizando FlatList e styled-components para estilização.

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,10 +1,25 @@
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
-import { Container, Form } from "./styles";
+import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Input } from "@components/Input";
+import { Filter } from "@components/Filter";
+import React, { useState } from "react";
+import { FlatList } from "react-native";
+import { PlayerCard } from "@components/PlayerCard";
+import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
-export function Players() {
+export function Hero() {
+  const [team, setTeam] = useState("time A");
+  const [players, setPlayers] = useState<string[]>([]);
+
+  const numberOfPlayers = players.length;
+
+  function playerRemove(name: string) {
+    setPlayers((prevState) => prevState.filter((player) => player !== name));
+  }
+
   return (
     <Container>
       <Header showBackButton />
@@ -16,6 +31,38 @@ export function Players() {
         <Input placeholder="Nome do participante" autoCorrect={false} />
         <ButtonIcon icon="add" />
       </Form>
+      <HeaderList>
+        <FlatList
+          data={["time A", "time B"]}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+        <NumberOfPlayers>{numberOfPlayers}</NumberOfPlayers>
+      </HeaderList>
+
+      <FlatList
+        data={players}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <PlayerCard name={item} onRemove={playerRemove} />
+        )}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Nenhum participante adicionado" />
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={[
+          { paddingBottom: 100 },
+          players.length === 0 && { flex: 1 },
+        ]}
+      />
+      <Button title="Remover turma" type="SECONDARY" />
     </Container>
   );
 }
