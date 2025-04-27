df9b84421c937a09316f65f25ba76bb10098b6c1
Daniel Fernando <daniel_fmpc@hotmail.com.br>
2025-04-27 15:06:22 -0300

    Atualiza tela de grupos para exibir mensagem quando a lista está vazia e adiciona botão para criar nova turma.

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,13 +4,11 @@ import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
 import { FlatList } from "react-native";
 import { useState } from "react";
+import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>([
-    "Turma 1",
-    "Turma 2",
-    "Turma 3",
-  ]);
+  const [groups, setGroups] = useState<string[]>([]);
   return (
     <Container>
       <Header />
@@ -19,7 +17,12 @@ export function Groups() {
         data={groups}
         keyExtractor={(item) => item}
         renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
       />
+      <Button title="Criar nova turma" />
     </Container>
   );
 }
