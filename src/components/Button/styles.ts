98dacd5a03ae3800a172c7b57c4f13286a2e5a6a
Daniel Fernando <daniel_fmpc@hotmail.com.br>
2025-04-27 14:08:31 -0300

    Adiciona componente de botão com estilos e tipagem para variantes PRIMARY e SECONDARY.

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,32 @@
+import { TouchableOpacity } from "react-native";
+import { styled } from "styled-components/native";
+
+export type ButtonTypeStyleProps = "PRIMARY" | "SECONDARY";
+
+const buttonVariant = {
+  PRIMARY: "GREEN_700",
+  SECONDARY: "RED_DARK",
+} as const;
+
+interface Props {
+  type: ButtonTypeStyleProps;
+}
+
+export const Container = styled(TouchableOpacity)<Props>`
+  flex: 1;
+
+  min-height: 56px;
+  max-height: 56px;
+
+  background-color: ${({ theme, type }) => theme.COLORS[buttonVariant[type]]};
+
+  border-radius: 6px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Title = styled.Text`
+  font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
+  color: ${({ theme }) => theme.COLORS.WHITE};
+  font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
+`;
