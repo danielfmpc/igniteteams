e16be3d8c354b286a71c5b01fc4238111580e8dc
Daniel Carlos <daniel_fmpc@hotmail.com.br>
2025-04-30 22:26:04 -0300

    Implementa a recuperação e exibição de grupos na tela Groups, utilizando useFocusEffect para atualizar a lista ao entrar na tela. Adiciona a funcionalidade de navegação para a tela de jogadores ao clicar em um grupo. Na tela NewGroup, implementa a criação de grupos com tratamento de erros e validação de entrada, melhorando a experiência do usuário ao adicionar novas turmas.

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -3,10 +3,11 @@ import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
 import { FlatList } from "react-native";
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { ListEmpty } from "@components/ListEmpty";
 import { Button } from "@components/Button";
-import { useNavigation } from "@react-navigation/native";
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
+import { groupGetAll } from "@storage/group/groupGetAll";
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
@@ -17,6 +18,25 @@ export function Groups() {
     navigation.navigate("newGroup");
   }
 
+  async function fetchGroups() {
+    try {
+      const data = await groupGetAll();
+      setGroups(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  function handleOpenGroup(group: string) {
+    navigation.navigate("players", { group });
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchGroups();
+    }, [])
+  );
+
   return (
     <Container>
       <Header />
@@ -24,7 +44,12 @@ export function Groups() {
       <FlatList
         data={groups}
         keyExtractor={(item) => item}
-        renderItem={({ item }) => <GroupCard title={item} />}
+        renderItem={({ item }) => (
+          <GroupCard
+            title={item}
+            onPress={() => handleOpenGroup(item)}
+          />
+        )}
         contentContainerStyle={groups.length === 0 && { flex: 1 }}
         ListEmptyComponent={() => (
           <ListEmpty message="Que tal cadastrar a primeira turma?" />
