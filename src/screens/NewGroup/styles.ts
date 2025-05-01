6a32a40356714bb851b122b7faa4cf9ae66d46cc
Daniel Carlos <daniel_fmpc@hotmail.com.br>
2025-04-30 22:27:40 -0300

    Replace View with SafeAreaView in NewGroup styles for improved mobile compatibility

--- src/screens/NewGroup/styles.ts
+++ src/screens/NewGroup/styles.ts
@@ -1,6 +1,7 @@
+import { SafeAreaView } from "react-native-safe-area-context";
 import { styled } from "styled-components/native";
 import { UsersThree } from "phosphor-react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+
 export const Container = styled(SafeAreaView)`
   flex: 1;
   background-color: ${({ theme }) => theme.COLORS.GRAY_600};
