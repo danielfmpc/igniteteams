3c3a3883bbeeee001ef80bdd45ca9f42f32a7f1f
Daniel Carlos <daniel_fmpc@hotmail.com.br>
2025-04-30 22:27:33 -0300

    Enhance Players screen with player addition and retrieval functionality
    - Added input handling for new player names, including validation and error alerts.
    - Implemented player addition to a specified group with the playerAddByGroup function.
    - Introduced fetching of players by team using playersGetByGroupAndTeam, updating the player list on team change.
    - Updated player management to handle PlayerStorageDto objects, ensuring proper data structure.

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { FlatList } from "react-native";
+import React, { useState, useEffect, useRef } from "react";
+import { Alert, FlatList, TextInput, Keyboard } from "react-native";
 import { useRoute } from "@react-navigation/native";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
@@ -10,31 +10,85 @@ import { Filter } from "@components/Filter";
 import { PlayerCard } from "@components/PlayerCard";
 import { ListEmpty } from "@components/ListEmpty";
 import { Button } from "@components/Button";
-
+import { playerAddByGroup } from "@storage/player/playersAddByGroup";
+import { PlayerStorageDto } from "@storage/player/PlayerStorageDTO";
+import { AppError } from "@utils/AppErro";
+import { playersGetByGroupAndTeam } from "@storage/player/playersGetByGroupAndTeam";
 interface RouteParams {
   group: string;
 }
 
 export function Players() {
+  const [newPlayer, setNewPlayer] = useState("");
   const [team, setTeam] = useState("time A");
-  const [players, setPlayers] = useState<string[]>([]);
-
-  const numberOfPlayers = players.length;
-
-  function playerRemove(name: string) {
-    setPlayers((prevState) => prevState.filter((player) => player !== name));
-  }
+  const [players, setPlayers] = useState<PlayerStorageDto[]>([]);
 
   const route = useRoute();
   const { group } = route.params as RouteParams;
+
+  const numberOfPlayers = players.length;
+
+  const newPlayerNameInputRef = useRef<TextInput>(null);
+
+  function playerRemove(name: string) {
+    setPlayers((prevState) => prevState.filter((player) => player.name !== name));
+  }
+
+  async function handlePlayerAdd() {
+    if (newPlayer.trim().length === 0) {
+      return Alert.alert("Novo participante", "O nome do participante é obrigatório.");
+    }
+
+    try {
+      const newPlayerDto: PlayerStorageDto = { name: newPlayer, team };
+
+      await playerAddByGroup(newPlayerDto, group);
+      newPlayerNameInputRef.current?.blur();
+      Keyboard.dismiss();
+
+      setNewPlayer("");
+      fetchPlayersByTeam();
+
+    } catch (error) {
+      if (error instanceof AppError) {
+        Alert.alert("Novo participante", error.message);
+      } else {
+        console.log(error);
+        Alert.alert("Novo participante", "Não foi possível adicionar o participante.");
+      }
+    }
+  }
+
+  async function fetchPlayersByTeam() {
+    try {
+      const players = await playersGetByGroupAndTeam(group, team);
+
+      setPlayers(players);
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Turma", "Não foi possível carregar os participantes.");
+    }
+  }
+
+  useEffect(() => {
+    fetchPlayersByTeam();
+  }, [team]);
 
   return (
     <Container>
       <Header showBackButton />
       <Highlight title={group} subtitle="adicione a galera e separe os times" />
       <Form>
-        <Input placeholder="Nome do participante" autoCorrect={false} />
-        <ButtonIcon icon="add" />
+        <Input
+          inputRef={newPlayerNameInputRef}
+          placeholder="Nome do participante"
+          autoCorrect={false}
+          onChangeText={setNewPlayer}
+          value={newPlayer}
+          onSubmitEditing={handlePlayerAdd}
+          returnKeyType="done"
+        />
+        <ButtonIcon icon="add" onPress={handlePlayerAdd} />
       </Form>
       <HeaderList>
         <FlatList
@@ -54,9 +108,9 @@ export function Players() {
 
       <FlatList
         data={players}
-        keyExtractor={(item) => item}
+        keyExtractor={(item) => item.name}
         renderItem={({ item }) => (
-          <PlayerCard name={item} onRemove={playerRemove} />
+          <PlayerCard name={item.name} onRemove={playerRemove} />
         )}
         ListEmptyComponent={() => (
           <ListEmpty message="Nenhum participante adicionado" />
