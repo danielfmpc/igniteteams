28be75b7d65f678739a4afcf9694e7f72f6a04dc
Daniel Carlos <daniel_fmpc@hotmail.com.br>
2025-04-30 22:27:23 -0300

    Add storage configuration for group and player collections
    - Introduced a new storageConfig object in storageConfig.ts to define constants for group and player collections, facilitating organized access to storage keys.

---
+++ src/storage/storageConfig.ts
@@ -0,0 +1,7 @@
+const GROUP_COLLECTION = "@ignite-teams:groups";
+const PLAYER_COLLECTION = "@ignite-teams:players";
+
+export const storageConfig = {
+  group: GROUP_COLLECTION,
+  player: PLAYER_COLLECTION,
+};
